11a484be91ced7d45fb4d64d2526a1a516f64435
Sebastian Iversen <kierstejn1994@gmail.com>
2021-07-10 18:56:59 +0200

    Changed arrow function to regular function

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 5000
 //Bubibap
 //selv bubibap
 
@@ -8,6 +8,6 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
+app.listen(port, function(){
   console.log(`Example app listening at http://localhost:${port}`)
 })
